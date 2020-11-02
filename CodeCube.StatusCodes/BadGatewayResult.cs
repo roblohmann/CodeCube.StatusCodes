0e0ecf153ca9a09b0f764ebda501ffbc53f27e1b
Rob Lohmann <rob@ecare.nl>
2020-11-02 11:52:12 +0100

    added inital code for alpha version

---
+++ CodeCube.StatusCodes/BadGatewayResult.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace CodeCube.StatusCodes
+{
+    public sealed class BadGatewayResult : StatusCodeResult
+    {
+        public BadGatewayResult() : base((int)HttpStatusCode.BadGateway)
+        {
+        }
+
+        public StatusCodeResult BadGateway()
+        {
+            return new StatusCodeResult((int)HttpStatusCode.BadGateway);
+        }
+
+        public ObjectResult BadGateway(object value)
+        {
+            return new ObjectResult(value)
+            {
+                StatusCode = (int)HttpStatusCode.BadGateway
+            };
+        }
+    }
+}
---
+++ CodeCube.StatusCodes/InternalServerErrorResult.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace CodeCube.StatusCodes
+{
+    public sealed class InternalServerErrorResult : StatusCodeResult
+    {
+        public InternalServerErrorResult() : base((int)HttpStatusCode.InternalServerError)
+        {
+        }
+
+        public StatusCodeResult InternalServerError()
+        {
+            return new StatusCodeResult((int)HttpStatusCode.InternalServerError);
+        }
+
+        public ObjectResult InternalServerError(object value)
+        {
+            return new ObjectResult(value)
+            {
+                StatusCode = (int)HttpStatusCode.InternalServerError
+            };
+        }
+    }
+}
